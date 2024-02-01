79b4c639bda9e32aa947a94510267ed5b6324970
pvillalva <pervillalva@gmail.com>
2024-02-01 14:47:09 -0300

    turn actor into a end-to-end notion to ghost solution

--- src/main.js
+++ src/main.js
@@ -1,12 +1,52 @@
-import { Actor, Dataset } from "apify";
-import { blocksToMD } from "./notion.js";
+import { Actor, Dataset, log } from "apify";
+import { blocksToMD, findDatabaseItems, updateItemStatus } from "./notion.js";
+import { createPost } from "./ghost.js";
 
 await Actor.init();
 
-const { notionToken, dbPageID } = await Actor.getInput();
+const {
+    notionToken,
+    notionDatabaseID,
+    notionFilterValue,
+    notionUpdatedValue,
+    ghostURL,
+    ghostKey,
+} = await Actor.getInput();
 
-const result = await blocksToMD(notionToken, dbPageID);
+try {
+    const pagesData = await findDatabaseItems(
+        notionDatabaseID,
+        notionToken,
+        notionFilterValue
+    );
 
-await Dataset.pushData(result);
+    for (let pageData of pagesData) {
+        const { pageId, title, authors } = pageData;
+
+        const result = await blocksToMD(notionToken, pageId);
+
+        await createPost(
+            ghostURL,
+            ghostKey,
+            result.articleContent,
+            title,
+            authors
+        );
+
+        log.info(`✅ "${title}" article was successfully created on Ghost.`);
+
+        if (notionUpdatedValue) {
+            await updateItemStatus(pageId, notionToken, notionUpdatedValue);
+
+            log.info(
+                `🔁 "${title}" item status updated from "${notionFilterValue}" to "${notionUpdatedValue}" in Notion.`
+            );
+        }
+
+        await Dataset.pushData(result);
+    }
+} catch (error) {
+    log.error("An error occurred:", error);
+}
 
 await Actor.exit();
--- src/notion.js
+++ src/notion.js
@@ -2,8 +2,65 @@ import { Client } from "@notionhq/client";
 import { NotionToMarkdown } from "notion-to-md";
 import showdown from "showdown";
 
+const STATUS_PROPERTY = "Status";
+
+function createNotionClient(token) {
+    return new Client({ auth: token });
+}
+
+/**
+ * Finds items in a Notion database that match a certain filter. If no filter is provided, it returns all items in the database.
+ */
+export async function findDatabaseItems(databaseId, token, filterValue) {
+    const notionClient = createNotionClient(token);
+
+    let query = {
+        database_id: databaseId,
+    };
+
+    if (filterValue) {
+        query.filter = {
+            property: STATUS_PROPERTY,
+            status: {
+                equals: filterValue,
+            },
+        };
+    }
+
+    const response = await notionClient.databases.query(query);
+
+    const pageInfo = response.results.map((item) => ({
+        pageId: item.id,
+        title: item.properties.Title.title[0].text.content,
+        authors: item.properties.Author.people.map((author) => author.name),
+    }));
+
+    return pageInfo;
+}
+
+/**
+ * Updates the status of an item in a Notion database.
+ */
+export async function updateItemStatus(pageId, token, updatedValue) {
+    const notionClient = createNotionClient(token);
+
+    await notionClient.pages.update({
+        page_id: pageId,
+        properties: {
+            [STATUS_PROPERTY]: {
+                status: {
+                    name: updatedValue,
+                },
+            },
+        },
+    });
+}
+
+/**
+ * Converts Notion blocks to Markdown.
+ */
 export async function blocksToMD(token, pageID) {
-    const notionClient = new Client({ auth: token });
+    const notionClient = createNotionClient(token);
 
     const n2m = new NotionToMarkdown({
         notionClient: notionClient,
@@ -13,9 +70,6 @@ export async function blocksToMD(token, pageID) {
     });
 
     const mdblocks = await n2m.pageToMarkdown(pageID);
-    mdblocks.forEach((block) =>
-        block.type === "table" ? console.log(block) : ""
-    );
     const mdString = n2m.toMarkdownString(mdblocks);
 
     const converter = new showdown.Converter();
