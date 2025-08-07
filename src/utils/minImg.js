c5bc7a4707ddc46f39251d9602875199568431ed
Percival Villalva <70678259+PerVillalva@users.noreply.github.com>
2025-08-07 16:37:28 +0400

    fix image order in Ghost output

--- src/utils/minImg.js
+++ src/utils/minImg.js
@@ -1,15 +1,20 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
 import sharp from "sharp";
-import { KeyValueStore } from "apify";
+import { KeyValueStore, log } from "apify";
 
 export async function optimizeImg(html, compressionMode) {
-    const $ = cheerio.load(html);
+    const $ = cheerio.load(html, null);
     const imgElements = $("img");
 
     for (let i = 0; i < imgElements.length; i++) {
         const img = imgElements[i];
         const imgUrl = $(img).attr("src");
+
+        if (!imgUrl) {
+            console.log(`Skipping image without src attribute at index ${i}`);
+            continue;
+        }
 
         // Extract the image name from the URL
         const imgName = $(img)
@@ -19,41 +24,51 @@ export async function optimizeImg(html, compressionMode) {
             .replace(/[^a-z0-9-]/g, "") // Remove all characters that are not a-z, 0-9, or -.
             .replace(/^-+|-+$/g, ""); // Remove leading and trailing hyphens.;
 
-        // Download the image
-        const response = await axios.get(imgUrl, {
-            responseType: "arraybuffer",
-        });
-        const cType = response.headers["content-type"];
+        try {
+            log.info(`Processing image: ${imgUrl}`);
 
-        // Skip SVG images
-        if (cType === "image/svg+xml") {
-            continue;
+            // Download the image
+            const response = await axios.get(imgUrl, {
+                responseType: "arraybuffer",
+            });
+            const cType = response.headers["content-type"];
+
+            // Skip SVG images
+            if (cType === "image/svg+xml") {
+                continue;
+            }
+            const imgBuffer = Buffer.from(response.data, "binary");
+
+            const isLossless = compressionMode === "lossless";
+
+            // Optimize and convert the image to webp
+            const optimizedImgBuffer = await sharp(imgBuffer, {
+                animated: true,
+            })
+                .webp({ lossless: isLossless }) // Convert the image to webp.
+                .toBuffer();
+
+            // Open KeyValueStore
+            const store = await KeyValueStore.open();
+
+            // Store the optimized image in the key-value store
+            await store.setValue(imgName, optimizedImgBuffer, {
+                contentType: "image/webp",
+            });
+
+            // Get saved image's URL
+            const optimizedImgUrl = store.getPublicUrl(imgName);
+
+            // Replace the original image URL with the URL of the optimized image
+            $(img).attr("src", optimizedImgUrl);
+
+            console.log(`Image optimized and replaced: ${imgUrl}`);
+        } catch (error) {
+            console.error(`Error processing image: ${imgUrl}`, error);
         }
-        const imgBuffer = Buffer.from(response.data, "binary");
-
-        const isLossless = compressionMode === "lossless";
-
-        // Optimize and convert the image to webp
-        const optimizedImgBuffer = await sharp(imgBuffer, { animated: true })
-            .webp({ lossless: isLossless }) // Convert the image to webp.
-            .toBuffer();
-
-        // Open KeyValueStore
-        const store = await KeyValueStore.open();
-
-        // Store the optimized image in the key-value store
-        await store.setValue(imgName, optimizedImgBuffer, {
-            contentType: "image/webp",
-        });
-
-        // Get saved image's URL
-        const optimizedImgUrl = store.getPublicUrl(imgName);
-
-        // Replace the original image URL with the URL of the optimized image
-        $(img).attr("src", optimizedImgUrl);
     }
 
-    const optimizedHTML = $("body").html();
+    const optimizedHTML = $.html();
 
     return optimizedHTML;
 }
